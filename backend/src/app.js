b8b68ad44b521d1f11a2a295d3db30b3dacad6a0
gulyaasjaanos <53534194+gulyaasjaanos@users.noreply.github.com>
2020-10-06 20:46:13 +0200

    fix: deployement

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 import api from './routes';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'static/')));
 app.use('/api', api);
 
 app.get('/*', function (req, res) {
-  res.status(200).send('running');
+  res.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
 export default app;
